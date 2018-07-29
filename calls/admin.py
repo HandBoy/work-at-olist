6db01d93121debe1fcecc9f58ff6ec0f5e712562
handboy <handersonmedeiros@gmail.com>
2018-07-29 12:32:00 -0300

    Creating models Call and add in admin

--- calls/admin.py
+++ calls/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Call, CallEnd, CallStart
 
 # Register your models here.
+admin.site.register(Call)
+admin.site.register(CallEnd)
+admin.site.register(CallStart)
--- calls/models.py
+++ calls/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
 # Create your models here.
+
+class Call(models.Model):
+    duration = models.DurationField()
+    price =  models.FloatField()
+
+class CallStart(models.Model):
+    TYPE = (
+    ('S', 'Start'),
+    ('E', 'End'),
+    )
+    timestamp = models.DateTimeField(auto_now_add=True)
+    call_id = models.ForeignKey(Call, on_delete=models.CASCADE, related_name='call_start')
+    source = models.CharField(max_length=20)
+    destination = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.source
+
+class CallEnd(models.Model):
+    TYPE = (
+    ('S', 'Start'),
+    ('E', 'End'),
+    )
+    timestamp = models.DateTimeField(auto_now_add=True)
+    call_id = models.ForeignKey(Call, on_delete=models.CASCADE, related_name='call_end')
+
+
+'''
+Call Start Record
+{
+  "id":  // Record unique identificator;
+  "type":  // Indicate if it's a call "start" or "end" record;
+  "timestamp":  // The timestamp of when the event occured;
+  "call_id":  // Unique for each call record pair;
+  "source":  // The subscriber phone number that originated the call;
+  "destination":  // The phone number receiving the call.
+}
+
+Call End Record
+{
+   "id":  // Record unique identificator;
+   "type":  // Indicate if it's a call "start" or "end" record;
+   "timestamp":  // The timestamp of when the event occured;
+   "call_id":  // Unique for each call record pair.
+}
+
+'''
