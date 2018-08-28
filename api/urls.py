535e4164e632439ae84b203899a61f39892bd93a
handboy <handersonmedeiros@gmail.com>
2018-08-28 13:59:56 -0300

    Removing unusual imports;
    Clean comments.

--- api/urls.py
+++ api/urls.py
@@ -1,18 +1,18 @@
 from django.conf.urls import url, include
-from rest_framework import routers, serializers, viewsets
-from .views import CallViewSet, CalculateCallViewSet, CreateCallViewSet, EndCallViewSet
+from rest_framework import routers
+from .views import CalculateCallViewSet, CreateCallViewSet, EndCallViewSet, MonthlyBillingView
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
-router.register(r'startcall', CallViewSet, base_name='startcall')
-router.register(r'createcall', CreateCallViewSet, base_name='createcall')
+router.register(r'startcall', CreateCallViewSet, base_name='startcall')
 router.register(r'endcall', EndCallViewSet, base_name='endcall')
 router.register(r'calculatecall', CalculateCallViewSet, base_name='calculatecall')
+
 app_name = 'api'
 
 urlpatterns = [
     url(r'^', include(router.urls)),
-
+    url(r'^billing/(?P<phone_number>[0-9]+)/$', MonthlyBillingView.as_view()),   
+    url(r'^billing/(?P<phone_number>[0-9]+)/(?P<year>[0-9]{4})/$', MonthlyBillingView.as_view()),
+    url(r'^billing/(?P<phone_number>[0-9]+)/(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/$', MonthlyBillingView.as_view()),   
 ]
-
-#    url(r'endcall', EndCallViewSet.as_view({"get": "retrieve", "post": "create", "put": "update", "patch": "partial_update", "delete": "destroy"}), {'get': 'list', 'post': 'create'}),
