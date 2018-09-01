d1c5de254e7b7dc54423c16b7f7edc04cb85da03
handboy <handersonmedeiros@gmail.com>
2018-09-01 14:09:58 -0300

    Add api documentation

--- phonebills/urls.py
+++ phonebills/urls.py
@@ -17,7 +17,12 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.urls import include, path
 
+from rest_framework.documentation import include_docs_urls
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'api/', include('api.urls')),
+    url(r'^docs/', include_docs_urls(
+        title='Phone Bills API',
+        description="API Endpoints Documentation"))
 ]
