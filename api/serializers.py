44eba83e0e62ca44b33f4feae486de5faaabb312
handboy <handersonmedeiros@gmail.com>
2018-09-01 18:42:39 -0300

    Document Serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,21 +1,30 @@
 from rest_framework import serializers
 
-from calls.models import Call, CallEnd, CallStart
+from calls.models import Call, CallStart
 
 
 class CallStartSerializer(serializers.ModelSerializer):
+    """
+    Class responsible for serializing CallStart instances
+    """
     class Meta:
         model = CallStart
         fields = ('source', 'destination')
 
 
 class CallSerializer(serializers.Serializer):
+    """
+    Class responsible for serializing Call instances
+    """
     class Meta:
         model = Call
         fields = '__all__'
 
 
 class MonthBillSerializer(serializers.Serializer):
+    """
+    Class responsible for serializing CallStart instances
+    """
     destination = serializers.CharField(max_length=200)
     date = serializers.CharField(max_length=200)
     time = serializers.CharField(max_length=200)
@@ -24,6 +33,10 @@ class MonthBillSerializer(serializers.Serializer):
 
 
 class CallAfterStartSerializer(serializers.Serializer):
+    """
+    Class responsible to serializing andreturn informations after
+    create a phone call
+    """
     call_id = serializers.IntegerField()
     source = serializers.CharField(max_length=200)
     destination = serializers.CharField(max_length=200)
