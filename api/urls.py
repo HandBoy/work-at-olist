d677186da6ee314067ede2a06f922c21f75e05a0
handboy <handersonmedeiros@gmail.com>
2018-09-01 15:14:49 -0300

    Removing unused imports
    Applying PEP8 standard line size

--- api/urls.py
+++ api/urls.py
@@ -1,20 +1,27 @@
 from django.conf.urls import include, url
 from rest_framework import routers
 
-from .views import (CalculateCallViewSet, CreateCallViewSet, EndCallViewSet,
-                    MonthlyBillingView)
+from .views import (CreateCallViewSet, EndCallViewSet, MonthlyBillingView)
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
-router.register(r'startcall', CreateCallViewSet, base_name='startcall')
-router.register(r'endcall', EndCallViewSet, base_name='endcall')
-router.register(r'calculatecall', CalculateCallViewSet, base_name='calculatecall')
 
 app_name = 'api'
 
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'^billing/(?P<phone_number>[0-9]+)/$', MonthlyBillingView.as_view()),   
-    url(r'^billing/(?P<phone_number>[0-9]+)/(?P<year>[0-9]{4})/$', MonthlyBillingView.as_view()),
-    url(r'^billing/(?P<phone_number>[0-9]+)/(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/$', MonthlyBillingView.as_view()),   
+    url(r'^startcall/$', CreateCallViewSet.as_view(), name='startcall'),
+    url(r'^endcall/$', EndCallViewSet.as_view(), name='endcall'),
+    url(r'^billing/'
+        + '(?P<phone_number>[0-9]+)/$',
+        MonthlyBillingView.as_view(), name='get_bill'),
+    url(r'^billing/'
+        + '(?P<phone_number>[0-9]+)/'
+        + '(?P<year>[0-9]{4})/$',
+        MonthlyBillingView.as_view(), name='get_bill'),
+    url(r'^billing/'
+        + '(?P<phone_number>[0-9]+)/'
+        + '(?P<year>[0-9]{4})/'
+        + '(?P<month>[0-9]{2})/$',
+        MonthlyBillingView.as_view(), name='get_bill'),
 ]
