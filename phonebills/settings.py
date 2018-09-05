20c7afddcd626bb4a0835b9db1b476cd90a4615b
handboy <handersonmedeiros@gmail.com>
2018-09-05 16:02:14 -0300

    again

--- phonebills/settings.py
+++ phonebills/settings.py
@@ -138,10 +138,6 @@ USE_TZ = False
 STATIC_ROOT = 'static'  # Important for Heroku
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 
