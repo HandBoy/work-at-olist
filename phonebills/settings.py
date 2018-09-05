53bf02d8e31ac42ca4968c6c4bca527453792cb5
handboy <handersonmedeiros@gmail.com>
2018-09-05 14:57:17 -0300

    remove not necessary config extra static file

--- phonebills/settings.py
+++ phonebills/settings.py
@@ -137,8 +137,3 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
