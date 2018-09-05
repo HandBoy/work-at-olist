cc25462fea9a4dc7533c899135cd7ce8d233ced3
handboy <handersonmedeiros@gmail.com>
2018-09-05 00:57:23 -0300

    Apply pattern restful in url resources

--- api/urls.py
+++ api/urls.py
@@ -6,22 +6,9 @@ from .views import (CreateCallViewSet, EndCallViewSet, MonthlyBillingView)
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
 
-app_name = 'api'
-
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'^startcall/$', CreateCallViewSet.as_view(), name='startcall'),
-    url(r'^endcall/$', EndCallViewSet.as_view(), name='endcall'),
-    url(r'^billing/'
-        + '(?P<phone_number>[0-9]+)/$',
-        MonthlyBillingView.as_view(), name='get_bill'),
-    url(r'^billing/'
-        + '(?P<phone_number>[0-9]+)/'
-        + '(?P<year>[0-9]{4})/$',
-        MonthlyBillingView.as_view(), name='get_bill'),
-    url(r'^billing/'
-        + '(?P<phone_number>[0-9]+)/'
-        + '(?P<year>[0-9]{4})/'
-        + '(?P<month>[0-9]{2})/$',
-        MonthlyBillingView.as_view(), name='get_bill'),
+    url(r'^call/start/$', CreateCallViewSet.as_view()),
+    url(r'^call/(?P<id>[0-9]+)/end/$', EndCallViewSet.as_view()),
+    url(r'^bills/(?P<phone_number>[0-9]+)/$', MonthlyBillingView.as_view())
 ]
